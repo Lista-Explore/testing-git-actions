bf9e92abf523852466da9d426b51891323efd065
Lista-Explore <lista@explore.ai>
2024-07-23 14:17:26 +0300

    ch

--- my_copier_template/run.py
+++ my_copier_template/run.py
@@ -1,4 +1,4 @@
 from copier import run_copy
 
 # Create a project from a local path
-run_copy("/Users/listaabutto/Documents/GitHub/testing-git-actions/my_copier_template/templates/Exercises.py.jinja", "/Users/listaabutto/Documents/GitHub/testing-git-actions/my_copier_template/regression-exercise.jinja")
+run_copy("\{\{project_name\}\}", "new_fold_2")
